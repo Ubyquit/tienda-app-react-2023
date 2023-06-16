1e06b69638f9be61f7f16c689cf0661667d0b667
Ubyquit <ingsystem.out.print@gmail.com>
2023-06-16 17:15:00 -0500

    fghjkl

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <div>
+        <a href='/'>Dashboard</a>
+        <a href='/fabricantes'>Fabricantes</a>
+        <a href='/productos'>Productos</a>
+    </div>
+  )
+}
